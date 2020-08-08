6092872469990785c6a896b4d7b0621c1981dee8
mohit9889 <raj.mohit9889@gmail.com>
2020-08-08 22:56:14 +0530

    Updates

---
+++ precache-manifest.c297b8feaa6111ac7be65930e00675ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c31c6fa639ea96ba7423ce5b0174619",
+    "url": "/coda-gloabl-frontend/index.html"
+  },
+  {
+    "revision": "a5c17cce337b792efa0d",
+    "url": "/coda-gloabl-frontend/static/css/main.d73fa5e2.chunk.css"
+  },
+  {
+    "revision": "4956eddd7c47a1af54ca",
+    "url": "/coda-gloabl-frontend/static/js/2.dc387051.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coda-gloabl-frontend/static/js/2.dc387051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c17cce337b792efa0d",
+    "url": "/coda-gloabl-frontend/static/js/main.5d888687.chunk.js"
+  },
+  {
+    "revision": "e5bd8dcb7552bfc2b5f7",
+    "url": "/coda-gloabl-frontend/static/js/runtime-main.efab1662.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/coda-gloabl-frontend/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/coda-gloabl-frontend/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "fd3a054f1abfa8be8cc81359718f3ca6",
+    "url": "/coda-gloabl-frontend/static/media/Illustration3.fd3a054f.png"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/coda-gloabl-frontend/static/media/Illustration4.df92c811.png"
+  },
+  {
+    "revision": "dea4998b081c6c1133a3b5b08ff2218c",
+    "url": "/coda-gloabl-frontend/static/media/Metropolis-Bold.dea4998b.otf"
+  },
+  {
+    "revision": "f7b5e589f88206b4bd5cb1408c5362e6",
+    "url": "/coda-gloabl-frontend/static/media/Metropolis-Regular.f7b5e589.otf"
+  },
+  {
+    "revision": "2556a4f74e2c523893e6928d6e300f1c",
+    "url": "/coda-gloabl-frontend/static/media/Metropolis-SemiBold.2556a4f7.otf"
+  }
+]);
